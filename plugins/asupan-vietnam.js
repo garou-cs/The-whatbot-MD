2d16b7bdb40b6196f20569ef15e880900eba9b0f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-08-28 13:23:59 -0300

    Delete plugins directory

--- plugins/asupan-vietnam.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/vietnam'
-    conn.sendButtonImg(m.chat, api-hyzer, 'aqui tiene', wm2, '𝚂𝚒𝚐𝚞𝚒𝚎𝚗𝚝𝚎', '.vietnam', m)
-}
-handler.help = ['vietnam']
-handler.tags = ['asupan']
-handler.command = /^(vietnam)$/i
-
-module.exports = handler
